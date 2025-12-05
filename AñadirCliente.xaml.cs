e215b437e4903c14ddd53eeed2fdc3b208e019cc
JESÚS MÁRQUEZ DUQUE <Jesus.Marquez1@alu.uclm.es>
2025-12-05 13:50:15 +0100

    Gestion de clientes, ventana implementada

---
+++ AñadirCliente.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TPV_Hosteleria
+{
+    /// <summary>
+    /// Lógica de interacción para AñadirCliente.xaml
+    /// </summary>
+    public partial class AñadirCliente : Window
+    {
+        public AñadirCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
