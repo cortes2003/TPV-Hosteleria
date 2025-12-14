4ebd31816b681f0856a91b89391762095967a06d
JESÚS MÁRQUEZ DUQUE <Jesus.Marquez1@alu.uclm.es>
2025-12-14 23:02:12 +0100

    Añadido el combo box del estado

---
+++ ItemComboBoxEstado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPV_Hosteleria
+{
+    internal class ItemComboBoxEstado
+    {
+        public String TipoEstado { get; set; }
+        public String ColorCirculo {  get; set; }
+        public String TextoComboBox => $"{TipoEstado} {ColorCirculo}";
+    }
+}
